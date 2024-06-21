1b02bbeb81a7021ff7f649ae2b4530d7c7055e80
庄润梓 <528185299@qq.com>
2024-06-21 17:16:04 +0800

    fix11

--- tests/e2e/greptimedbcluster_baremetal_test.go
+++ tests/e2e/greptimedbcluster_baremetal_test.go
@@ -38,7 +38,7 @@ var _ = Describe("Basic test of greptimedb cluster", func() {
 		}()
 
 		for {
-			if conn, err := net.DialTimeout("http", "localhost:4000", 2*time.Second); err == nil {
+			if conn, err := net.DialTimeout("tcp", "localhost:4000", 2*time.Second); err == nil {
 				defer conn.Close()
 				break
 			}
@@ -47,8 +47,8 @@ var _ = Describe("Basic test of greptimedb cluster", func() {
 		err = getClusterinBaremetal()
 		Expect(err).NotTo(HaveOccurred(), "failed to get cluster in baremetal")
 
-		err = cmd.Cancel()
-		Expect(err).NotTo(HaveOccurred(), "failed to exit create cluster process")
+		err = cmd.Process.Kill()
+		Expect(err).NotTo(HaveOccurred(), "failed to kill create cluster process")
 
 		err = deleteClusterinBaremetal()
 		Expect(err).NotTo(HaveOccurred(), "failed to delete cluster in baremetal")
