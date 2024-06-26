5f37a4b67b8600f7000f1f2c1a75ceb1635f5616
庄润梓 <528185299@qq.com>
2024-06-26 11:29:04 +0800

    fix

--- tests/e2e/greptimedbcluster_baremetal_test.go
+++ tests/e2e/greptimedbcluster_baremetal_test.go
@@ -56,10 +56,10 @@ var _ = Describe("Basic test of greptimedb cluster in baremetal", func() {
 			if exitErr, ok := err.(*exec.ExitError); ok {
 				GinkgoWriter.Printf("Process was killed with signal: %v,the process is terminated successfully\n", exitErr)
 			} else {
-				GinkgoWriter.Printf("Process terminated with error: %v,failed to terminater the process\n", err)
+				GinkgoWriter.Printf("Process terminated with error: %v,failed to terminated the process\n", err)
 			}
 		} else {
-			GinkgoWriter.Printf("failed to terminater the process\n")
+			GinkgoWriter.Printf("failed to terminated the process\n")
 		}
 
 		err = deleteClusterinBaremetal()
