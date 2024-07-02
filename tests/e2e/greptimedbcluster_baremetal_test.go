ab43e0b6ced9b619b8e277a10a5c87ffa1856b43
庄润梓 <528185299@qq.com>
2024-07-02 15:23:55 +0800

    baremetalport

--- tests/e2e/greptimedbcluster_baremetal_test.go
+++ tests/e2e/greptimedbcluster_baremetal_test.go
@@ -26,7 +26,6 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"k8s.io/klog/v2"
 )
 
 var _ = Describe("Basic test of greptimedb cluster in baremetal", Ordered, func() {
@@ -41,10 +40,6 @@ var _ = Describe("Basic test of greptimedb cluster in baremetal", Ordered, func(
 	It("Bootstrap cluster in baremteal", func() {
 		var err error
 		createcmd := newCreateClusterinBaremetalCommand()
-
-		go func() {
-			forwardRequestinbaremetal()
-		}()
 
 		err = createcmd.Start()
 		Expect(err).NotTo(HaveOccurred(), "failed to create cluster in baremetal")
@@ -166,13 +161,3 @@ func killProcess(pid int) error {
 	return err
 }
 */
-
-func forwardRequestinbaremetal() {
-	for {
-		cmd := exec.Command("kubectl", "port-forward", "svc/mydb-frontend", "4002:4002")
-		if err := cmd.Run(); err != nil {
-			klog.Errorf("Failed to port forward: %v", err)
-			return
-		}
-	}
-}
